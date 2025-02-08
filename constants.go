514d1a766cac41c377a8b504754632955c676c2f
James Herbstritt <james.herbstritt@gmail.com>
2025-02-07 18:33:36 -0800

    Add more constants

--- constants.go
+++ constants.go
@@ -1,5 +1,6 @@
 package main
 
+const FITBIT_BASE_URL = "https://api.fitbit.com/1"
 const GetProfileUrl = "https://api.fitbit.com/1/user/%s/profile.json"
 const GetLifetimeStatsUrl = "https://api.fitbit.com/1/user/%s/activities.json"
 const GetBadgesUrl = "https://api.fitbit.com/1/user/%s/badges.json"
