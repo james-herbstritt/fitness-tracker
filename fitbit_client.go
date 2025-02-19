2bb1debd95cf3ae163f5fbb46c2f7308ef1db281
James Herbstritt <james.herbstritt@gmail.com>
2025-02-18 20:31:56 -0800

    Add GetProfile to client

--- fitbit_client.go
+++ fitbit_client.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
 	"net/http"
 	"net/url"
 	"time"
@@ -50,14 +48,12 @@ func (c *FitbitClient) GetActivities(ctx context.Context, params map[string]stri
 	path := fmt.Sprintf(GET_ACTIVITY_LOG_LIST_PATH, "-")
 	u := c.BaseURL.JoinPath(path)
 
-	fmt.Println(u)
 	req, err := http.NewRequest("GET", u.String(), nil)
 	if err != nil {
 		return nil, err
 	}
 
 	addQueryParamsToRequest(req, params)
-	fmt.Println(req.URL.String())
 	resp, err := c.doRequest(ctx, req)
 	if err != nil {
 		return nil, err
@@ -76,38 +72,29 @@ func (c *FitbitClient) GetActivities(ctx context.Context, params map[string]stri
 	return result, nil
 }
 
-// the return http request might be a Fitbit request?
-// func (c *FitbitClient) newRequest(method string, path string, body interface{}) (*http.Request, error) {
-//     rel := &url.URL{Path: path}
-//     u := c.BaseURL.ResolveReference(rel)
+func (c *FitbitClient) GetProfile(ctx context.Context) (*Profile, error) {
+	path := fmt.Sprintf(GET_PROFILE_PATH, "-")
+	u := c.BaseURL.JoinPath(path)
 
-//     if (body == nil) {
-//        return nil, errors.New("body argument to newRequest was nil")
-//     }
-//     encodedBody, err := encodeBody(body)
-//     if (err != nil) {
-//         return nil, err
-//     }
-//     req, err := http.NewRequest(method, u.String(), encodedBody)
-//     if err != nil {
-//         return nil, err
-//     }
+	req, err := http.NewRequest("GET", u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
 
-//     // TODO:: Make this cleaner somehow
-//     bearerToken := fmt.Sprintf("Bearer %s", c.AccessToken)
-// 	header := http.Header{
-// 		"accept":          {"application/json"},
-// 		"accept-langauge": {"en_US"},
-// 		"accept-locale":   {"en_US"},
-// 		"Authorization":   {bearerToken},
-// 	}
+	resp, err := c.doRequest(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
 
-//     req.Header = header
-//     return req, err
-// }
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New("failed to fetch profile: " + resp.Status)
+	}
 
-func encodeBody(body interface{}) (io.ReadWriter, error) {
-	buf := new(bytes.Buffer)
-	err := json.NewEncoder(buf).Encode(body)
-	return buf, err
+	var result *Profile
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return nil, err
+	}
+
+	return result, nil
 }
