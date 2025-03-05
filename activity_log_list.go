0925723e366f9c926d9182a156d016e2b7a41be1
James Herbstritt <james.herbstritt@gmail.com>
2025-03-04 20:52:00 -0800

    Remove old client structure

--- activity_log_list.go
+++ activity_log_list.go
@@ -19,58 +19,42 @@ func (t *ActivityTime) UnmarshalJSON(b []byte) (err error) {
 }
 
 type ActivityLevelSection struct {
-	Minutes int64 `json:"minutes,omitempty"`
-	Name string `json:"name,omitempty"`
+	Minutes int64  `json:"minutes,omitempty"`
+	Name    string `json:"name,omitempty"`
 }
 
 type Activity struct {
-	ActivityDuration int64 `json:"activityDuration,omitempty"`
-	ActivityLevel []ActivityLevelSection `json:"activityLevel,omitempty"`
-	ActivityName string `json:"activityName,omitempty"`
-	ActivityTypeId int64 `json:"activityTypeId,omitempty"`
-	Calories int64 `json:"calories,omitempty"`
-	CaloriesLink string `json:"caloriesLink,omitempty"`
-	Duration int64 `json:"duration,omitempty"`
-	ElevationGain float64 `json:"elevationGain,omitempty"`
-	LastModified ActivityTime `json:"lastModified,omitempty"`
-	LogId int64 `json:"logId,omitempty"`
-	LogType string `json:"logType,omitempty"`
+	ActivityDuration      int64                  `json:"activityDuration,omitempty"`
+	ActivityLevel         []ActivityLevelSection `json:"activityLevel,omitempty"`
+	ActivityName          string                 `json:"activityName,omitempty"`
+	ActivityTypeId        int64                  `json:"activityTypeId,omitempty"`
+	Calories              int64                  `json:"calories,omitempty"`
+	CaloriesLink          string                 `json:"caloriesLink,omitempty"`
+	Duration              int64                  `json:"duration,omitempty"`
+	ElevationGain         float64                `json:"elevationGain,omitempty"`
+	LastModified          ActivityTime           `json:"lastModified,omitempty"`
+	LogId                 int64                  `json:"logId,omitempty"`
+	LogType               string                 `json:"logType,omitempty"`
 	ManualValuesSpecified struct {
 		Calories bool `json:"calories,omitempty"`
 		Distance bool `json:"distance,omitempty"`
-		Steps bool `json:"steps,omitempty"`
+		Steps    bool `json:"steps,omitempty"`
 	}
-	OriginalDuration int64 `json:"originalDuration,omitempty"`
+	OriginalDuration  int64        `json:"originalDuration,omitempty"`
 	OriginalStartTime ActivityTime `json:"originalStartTime,omitempty"`
-	StartTime ActivityTime `json:"startTime,omitempty"`
-	Steps int64 `json:"steps,omitempty"`
-	TcxLink string `json:"tcxLink,omitempty"`
+	StartTime         ActivityTime `json:"startTime,omitempty"`
+	Steps             int64        `json:"steps,omitempty"`
+	TcxLink           string       `json:"tcxLink,omitempty"`
 }
 
 type ActivityLogList struct {
 	Activities []Activity `json:"activities,omitempty"`
 	Pagination struct {
 		AfterDate ProfileTime `json:"afterDate,omitempty"`
-		Limit int64 `json:"limit,omitempty"`
-		Next string `json:"next,omitempty"`
-		Offset int64 `json:"offset,omitempty"`
-		Previous string `json:"previous,omitempty"`
-		Sort string `json:"sort,omitempty"`
+		Limit     int64       `json:"limit,omitempty"`
+		Next      string      `json:"next,omitempty"`
+		Offset    int64       `json:"offset,omitempty"`
+		Previous  string      `json:"previous,omitempty"`
+		Sort      string      `json:"sort,omitempty"`
 	} `json:"pagination,omitempty"`
-}
-
-func GetActivityLogList(userId string, accessToken string, params map[string]string) *ActivityLogList {
-	url, header := MakeUrlAndHeader(GetActivityLogListUrl, accessToken, userId)
-	res := GetWithQueryParams(url, header, params)
-	var activityLogList ActivityLogList
-	ProcessResponseBody(res, &activityLogList)
-	return &activityLogList
-}
-
-func GetActivityLogListWithContext(c context.Context, userId string, accessToken string, params map[string]string) *ActivityLogList {
-	url, header := MakeUrlAndHeader(GetActivityLogListUrl, accessToken, userId)
-	res := GetWithContextWithQueryParams(c, url, header, params)
-	var activityLogList ActivityLogList
-	ProcessResponseBody(res, &activityLogList)
-	return &activityLogList
 }
--- lifetime_stats.go
+++ lifetime_stats.go
@@ -22,57 +22,41 @@ type LifetimeStats struct {
 	Best struct {
 		Total struct {
 			Distance struct {
-				Date TotalTime `json:"date,omitempty"`
-				Value float64 `json:"value,omitempty"`
+				Date  TotalTime `json:"date,omitempty"`
+				Value float64   `json:"value,omitempty"`
 			} `json:"distance"`
 			Floors struct {
-				Date TotalTime `json:"date,omitempty"`
-				Value float64 `json:"value,omitempty"`
+				Date  TotalTime `json:"date,omitempty"`
+				Value float64   `json:"value,omitempty"`
 			} `json:"floors"`
 			Steps struct {
-				Date TotalTime `json:"date,omitempty"`
-				Value int64 `json:"value,omitempty"`
+				Date  TotalTime `json:"date,omitempty"`
+				Value int64     `json:"value,omitempty"`
 			} `json:"steps"`
 		} `json:"total"`
 		Tracker struct {
 			Distance struct {
-				Date TotalTime `json:"date"`
-				Value float64 `json:"value"`
+				Date  TotalTime `json:"date"`
+				Value float64   `json:"value"`
 			} `json:"distance"`
 			Floors struct {
-				Date TotalTime `json:"date"`
-				Value float64 `json:"value"`
+				Date  TotalTime `json:"date"`
+				Value float64   `json:"value"`
 			} `json:"floors"`
 			Steps struct {
-				Date TotalTime `json:"date"`
-				Value int64 `json:"value"`
+				Date  TotalTime `json:"date"`
+				Value int64     `json:"value"`
 			} `json:"steps"`
 		} `json:"tracker"`
 	} `json:"best"`
 	Lifetime struct {
 		Total struct {
 			// ActiveScore and CaloriesOut are here for backwards compatibility, will always be -1
-			ActiveScore int64 `json:"activeScore"`
-			CaloriesOut int64 `json:"caloriesOut"`
-			Distance float64 `json:"distance"`
-			Floors float64 `json:"floors"`
-			Steps int64 `json:"steps"`
+			ActiveScore int64   `json:"activeScore"`
+			CaloriesOut int64   `json:"caloriesOut"`
+			Distance    float64 `json:"distance"`
+			Floors      float64 `json:"floors"`
+			Steps       int64   `json:"steps"`
 		}
 	} `json:"lifetime"`
-}
-
-func GetLifetimeStats(userId string, accessToken string) *LifetimeStats {
-	url, header := MakeUrlAndHeader(GetLifetimeStatsUrl, accessToken, userId)
-	res := Get(url, header)
-	var lifetimeStats LifetimeStats
-	ProcessResponseBody(res, &lifetimeStats)
-	return &lifetimeStats
-}
-
-func GetLifetimeStatsWithContext(c context.Context, userId string, accessToken string) *LifetimeStats {
-	url, header := MakeUrlAndHeader(GetLifetimeStatsUrl, accessToken, userId)
-	res := GetWithContext(c, url, header)
-	var lifetimeStats LifetimeStats
-	ProcessResponseBody(res, &lifetimeStats)
-	return &lifetimeStats
 }
